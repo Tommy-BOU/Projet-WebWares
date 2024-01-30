76cbf5a650513bcd724f729f0999d1a5baa44cbb
Tommy-BOU <tommy.boubaker@free.fr>
2024-01-30 22:00:49 +0100

    Mise en place du store et suppression des views inutiles

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex'
+
+
+export default createStore({
+  state: {
+    // Etat global -> propriété de données partagées par tous les composants 
+   
+  },
+  mutations: {
+    // Mutations -> méthodes qui modifient les propriétés de l'état global, synchrone
+
+  },
+  actions: {
+    // Actions -> méthodes asynchrone
+  },
+  getters: {
+    // Getters -> propriétés calculées partagées par tous les composants ( computed)
+  },
+})
